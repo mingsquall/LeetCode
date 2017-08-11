4c432082e31e9cc66b3dfdf3686c159bbc0b4483
YuanMing Liu <774166816@qq.com>
2017-08-11 15:15:06 +0800

    update no.151 answer
    The question is no.151.

--- Reverse Words in a String.py
+++ Reverse Words in a String.py
@@ -1,3 +1,10 @@
+"""
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+"""
 class Solution(object):
     def reverseWords(self, s):
         """
@@ -7,4 +14,18 @@ class Solution(object):
         # return ' '.join(s.split()[::-1])
         return ' '.join(reversed(s.split()))
 s = Solution()
+print(s.reverseWords('I love u'))
+
+class Solution1(object):
+    def reverseWords(self, s):
+        i = 0
+        result = ''
+        for j in range(len(s) + 1):
+            if j == len(s) or s[j] == ' ':
+                if i < j:
+                    result = s[i:j] + ' ' + result
+                i = j + 1
+        return result.strip()
+
+s = Solution1()
 print(s.reverseWords('I love u'))
